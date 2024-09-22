14ce1a581139f83a1e52610f99d5687004025184
Enzo FLOCH <enzofloch37210@gmail.com>
2024-09-22 18:51:27 +0200

    tri par categorie

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,18 +1,19 @@
-/*   Variable   */
+/**************   Variable   ***************/
 
 let works = [];
-console.log("🚀 ~ works:", works);
+//console.log("🚀 ~ works:", works);
 let sectionPortfolio = document.getElementById("portfolio");
 let gallery = document.querySelector(".gallery");
+let filters = document.querySelector(".filters");
 
-/* Fetch Works */
-const myFetch = async () => {
+/****************  Fetch Works ****************/
+const getWorks = async () => {
   try {
     const response = await fetch("http://localhost:5678/api/works");
-    console.log("🚀 ~ myFetch ~ response:", response);
+    //console.log("🚀 ~ getWorks ~ response:", response);
     if (response.ok) {
       const result = await response.json();
-      console.log("🚀 ~ myFetch ~ result:", result);
+      //console.log("🚀 ~ getWorks ~ result:", result);
       works = result;
       return result;
     } else {
@@ -22,11 +23,15 @@ const myFetch = async () => {
     console.log(error);
   }
 };
-
-/* Display Work  */
+getWorks().then((data) => {
+  data.forEach((work) => {
+    displayWork(work);
+  });
+});
+/***********  Display Work  **************/
 
 const displayWork = (oneWork) => {
-  console.log("🚀 ~ displayWork ~ oneWork:", oneWork);
+  //console.log("🚀 ~ displayWork ~ oneWork:", oneWork);
   let figure = document.createElement("figure");
   let img = document.createElement("img");
   img.src = oneWork.imageUrl;
@@ -36,8 +41,51 @@ const displayWork = (oneWork) => {
   figure.appendChild(figcaption);
   gallery.appendChild(figure);
 };
-myFetch().then((data) => {
-  data.forEach((work) => {
-    displayWork(work);
+
+/*******    Display Button Categories     ********/
+
+/******* Fetch Categories *********/
+
+async function getCategories() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  return await response.json();
+}
+
+async function displayCategoriesButtons() {
+  const categories = await getCategories();
+  //console.log(categories);
+  categories.forEach((oneCategorie) => {
+    const btn = document.createElement("button");
+    btn.textContent = oneCategorie.name;
+    btn.id = oneCategorie.id;
+    filters.appendChild(btn);
   });
-});
+}
+displayCategoriesButtons();
+
+/******* Filtrages des buttons par categories ********/
+
+async function filterCategorie() {
+  const allWorks = await getWorks();
+  //console.log(allWorks);
+  const buttons = document.querySelectorAll(".filters button");
+  //console.log(buttons);
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      btnId = e.target.id; /**Recuper l'ID au click **/
+      gallery.innerHTML = ""; /** Supprime les photos au click **/
+      if (btnId !== "0") {
+        const allWorksTriCategorie = allWorks.filter((oneWork) => {
+          /**Trouver meilleur nom pour "all works / allWorksTri" ect */
+          return oneWork.categoryId == btnId;
+        });
+        allWorksTriCategorie.forEach((oneWork) => {
+          displayWork(oneWork);
+        });
+      } else {
+      }
+      console.log(btnId);
+    });
+  });
+}
+filterCategorie();
